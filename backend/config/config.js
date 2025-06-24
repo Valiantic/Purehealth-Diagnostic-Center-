125ff5c3c4ab7acad6694f300c3bc4bbd4582b35
StevenMadali <stevenmadali17@gmail.com>
2025-06-24 13:24:17 +0800

    Modify: Restrict CORS and Database Config

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'devpurehealthdb',
+    database: process.env.DB_NAME || 'purehealthdb',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
   },
