a81671b9965388c30f657a45ff9e9e7e18b7aa03
StevenMadali <stevenmadali17@gmail.com>
2025-06-24 18:57:54 +0800

    Add: Vercel Route JSON and Dialect Note on Backend

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'purehealthdb',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'postgresql',
+    dialect: 'postgresql', // change this to mysql if local development 
   },
   test: {
     // Test configuration
